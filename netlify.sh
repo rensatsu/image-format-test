34b53f8a81efec4e54550c7e69d6e39178e3d0ae
Pavel Tvaladze <reddragonsportal@gmail.com>
2021-12-14 20:39:10 +0300

    Use Github Actions to build

--- netlify.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-set -o pipefail
-set -o nounset
-
-curl -L https://download.imagemagick.org/ImageMagick/download/binaries/magick -o magick.AppImage
-
-alias convert='./magick.AppImage'
-alias magick='./magick.AppImage'
