ef86378178d12e3f0d73caa9eab63740c5b4eb7e
Pavel Tvaladze <reddragonsportal@gmail.com>
2021-12-14 19:51:16 +0300

    Fix netlify build

---
+++ netlify.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+curl -L https://download.imagemagick.org/ImageMagick/download/binaries/magick -o magick.AppImage
+
+alias convert = ./magick.AppImage
+alias magick = ./magick.AppImage
